c7f6203b396ecc35bae9aacd84049c318bb6ca5b
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-17 22:19:14 +0800

    feat(utils): adds function to get search params in url string

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getSearchParam(url: string, param: string) {
+  const include = url.includes(param);
+
+  if (!include) return null;
+
+  const params = url.split(/([&,?,=])/);
+  const index = params.indexOf(param);
+  const value = params[index + 2];
+  return value;
+}
