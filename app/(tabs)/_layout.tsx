903e1c7c4f580035b100e19ab7d1207b5d8ce6fc
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-18 02:09:08 +0800

    feat(settings): adds settings page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ import { useColorScheme } from '~/lib/useColorScheme';
 import Icon from '~/lib/icons/Icon';
 import { NAV_THEME } from '~/lib/constants';
 import ThemeToggle from '~/components/theme-toggle';
+import { cn } from '~/lib/utils';
 
 export default function TabLayout() {
   const { colorScheme } = useColorScheme();
@@ -24,14 +25,27 @@ export default function TabLayout() {
         name="index"
         options={{
           headerTitle: 'Library',
-          tabBarIcon: ({ color }) => <Icon name="Library" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Icon name="Library" className={cn(focused ? 'text-foreground' : 'text-primary')} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="browse"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <Icon name="Compass" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <Icon name="Compass" className={cn(focused ? 'text-foreground' : 'text-primary')} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ focused }) => (
+            <Icon name="Settings" className={cn(focused ? 'text-foreground' : 'text-primary')} />
+          ),
         }}
       />
     </Tabs>
