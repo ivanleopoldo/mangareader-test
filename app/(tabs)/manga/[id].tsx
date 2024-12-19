ef18b28c644b63d3194c22bb16d378b1ef76676a
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-19 14:21:28 +0800

    feat(manga): displays image

--- app/(tabs)/manga/[id].tsx
+++ app/(tabs)/manga/[id].tsx
@@ -3,13 +3,15 @@ import { Text, View } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { useQuery } from '@tanstack/react-query';
 import { MangaService } from '~/api/service/manga';
+import { MangaDetails } from '~/lib/models';
+import Image from '~/components/native/Image';
 
 export default function Manga() {
   const params = useLocalSearchParams();
   const { data, isLoading } = useQuery({
     queryKey: ['manga', params.id],
     queryFn: async () => {
-      const res = await MangaService.getManga(params?.url.toString());
+      const res = await MangaService.getManga(params?.url.toString(), params as MangaDetails);
       return res;
     },
   });
@@ -18,8 +20,16 @@ export default function Manga() {
 
   return (
     <View>
-      <Text>{params?.title}</Text>
-      <Text>{data?.summary}</Text>
+      <Image
+        className="h-20 w-20"
+        source={{
+          uri: params.cover,
+          cacheKey: params.id,
+        }}
+        cachePolicy={'memory-disk'}
+      />
+      <Text className="text-foreground">{data?.details.title}</Text>
+      <Text className="text-foregroun:">{data?.details.summary}</Text>
     </View>
   );
 }
